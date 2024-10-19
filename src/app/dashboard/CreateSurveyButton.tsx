f956e15cbfea4127b2183cc8562dc65d805f43b7
Shourya Naik <sjnaik02@gmail.com>
2024-10-19 10:24:21 +0530

    added a loading state on form creation

--- src/app/dashboard/CreateSurveyButton.tsx
+++ src/app/dashboard/CreateSurveyButton.tsx
@@ -1,25 +1,34 @@
 "use client";
 
 import { Button } from "@/components/ui/button";
-import { PlusCircle } from "lucide-react";
+import { PlusCircle, Loader2 } from "lucide-react";
 import { useRouter } from "next/navigation";
 import { createSurvey } from "@/server/queries";
+import { useState } from "react";
 
 export const CreateFormButton = () => {
+  const [loading, setLoading] = useState(false);
   const router = useRouter();
   const handleCreateForm = async () => {
+    setLoading(true);
     const newSurvey = await createSurvey();
-
     router.push(`/survey/create/${newSurvey?.id}`);
   };
 
   return (
-    <Button
-      className="flex items-center rounded-2xl px-4 py-2 text-sm"
-      onClick={handleCreateForm}
-    >
-      <PlusCircle className="mr-2 h-4 w-4" />
-      Create Survey
-    </Button>
+    <>
+      <Button
+        className="flex items-center rounded-2xl px-4 py-2 text-sm"
+        onClick={handleCreateForm}
+      >
+        <PlusCircle className="mr-2 h-4 w-4" />
+        Create Survey
+      </Button>
+      {loading && (
+        <div className="absolute flex h-full w-full items-center justify-center bg-white">
+          <Loader2 className="h-4 w-4 animate-spin" />
+        </div>
+      )}
+    </>
   );
 };
