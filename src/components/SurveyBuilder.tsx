71a9caf0a7cb179e2f74262175251bef236bd32e
Shourya Naik <sjnaik02@gmail.com>
2024-10-21 20:42:55 +0530

    Added a slightly nicer ui for managing elements

--- src/components/SurveyBuilder.tsx
+++ src/components/SurveyBuilder.tsx
@@ -3,8 +3,9 @@
 import useSurveyBuilder from "./hooks/useSurveyBuilder";
 import { Button } from "./ui/button";
 import { TextArea } from "./fields/TextArea";
-import { SurveyElements } from "./SurveyElement";
-import { Trash } from "lucide-react";
+import { MultipleChoice } from "./fields/MultipleChoice";
+import { SurveyElements, SurveyElementInstance } from "./SurveyElement";
+import { Trash, CirclePlus } from "lucide-react";
 import { Switch } from "./ui/switch";
 
 const SurveyBuilder: React.FC = () => {
@@ -13,40 +14,30 @@ const SurveyBuilder: React.FC = () => {
 
   return (
     <div className="flex flex-col gap-4">
-      <h1 className="text-2xl">Survey Builder</h1>
-      <Button onClick={() => addElement(0, TextArea.construct(randomId()))}>
-        Add TextArea
-      </Button>
       <div className="mx-auto flex w-full max-w-2xl flex-col gap-4">
+        <h1 className="text-2xl">Survey Builder</h1>
         {elements.map((element) => (
-          <div key={element.id} className="flex flex-col items-center gap-2">
-            {SurveyElements[element.type].editorComponent({
-              elementInstance: element,
-            })}
-            <div className="flex w-full justify-between gap-2">
-              <Button
-                variant="destructive"
-                size="icon"
-                onClick={() => removeElement(element.id)}
-              >
-                <Trash className="h-4 w-4" />
-              </Button>
-              <Switch
-                checked={element.properties?.required}
-                onCheckedChange={(checked) => {
-                  updateElement(element.id, {
-                    id: element.id,
-                    type: element.type,
-                    properties: {
-                      ...element.properties,
-                      required: checked,
-                    },
-                  });
-                }}
-              />
-            </div>
-          </div>
+          <BuilderElementWrapper
+            element={element}
+            removeElement={removeElement}
+            updateElement={updateElement}
+          />
         ))}
+        <Button
+          onClick={() => addElement(0, TextArea.construct(randomId()))}
+          variant="secondary"
+          className="flex w-fit items-center border-2 border-dashed text-lg"
+        >
+          <CirclePlus className="mr-1 h-8 w-8" />
+          Add TextArea
+        </Button>
+        <Button
+          onClick={() => addElement(0, MultipleChoice.construct(randomId()))}
+          variant="secondary"
+          className="flex w-fit items-center border-2 border-dashed text-lg"
+        >
+          Add Multiple Choice
+        </Button>
       </div>
     </div>
   );
@@ -56,4 +47,45 @@ export default SurveyBuilder;
 
 const randomId = () => {
   return Date.now().toString() + Math.random().toString(36).substring(2, 15);
+};
+
+const BuilderElementWrapper: React.FC<{
+  element: SurveyElementInstance;
+  removeElement: (id: string) => void;
+  updateElement: (id: string, element: SurveyElementInstance) => void;
+}> = ({ element, removeElement, updateElement }) => {
+  return (
+    <div
+      key={element.id}
+      className="flex flex-col items-center gap-2 border-2 border-muted p-4"
+    >
+      {SurveyElements[element.type].editorComponent({
+        elementInstance: element,
+      })}
+
+      <div className="flex w-full items-center justify-between gap-2">
+        <Button
+          variant="destructive"
+          onClick={() => removeElement(element.id)}
+          className="px-2 py-1"
+        >
+          <Trash className="mr-1 h-4 w-4" />
+          Remove
+        </Button>
+        <Switch
+          checked={element.properties?.required}
+          onCheckedChange={(checked) => {
+            updateElement(element.id, {
+              id: element.id,
+              type: element.type,
+              properties: {
+                ...element.properties,
+                required: checked,
+              },
+            });
+          }}
+        />
+      </div>
+    </div>
+  );
 };
