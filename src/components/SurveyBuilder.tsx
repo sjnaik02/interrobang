a96f76b4ce063daf4ea2934a396f2b7eb3286bea
Shourya Naik <sjnaik02@gmail.com>
2024-10-22 19:54:38 +0530

    made the editor ui more slick

--- src/components/SurveyBuilder.tsx
+++ src/components/SurveyBuilder.tsx
@@ -3,17 +3,30 @@
 import useSurveyBuilder from "./hooks/useSurveyBuilder";
 import { Button } from "./ui/button";
 import { TextArea } from "./fields/TextArea";
+import { Label } from "./ui/label";
+import { cn } from "@/lib/utils";
 import { MultipleChoice } from "./fields/MultipleChoice";
 import { SurveyElements, SurveyElementInstance } from "./SurveyElement";
 import { Trash, CirclePlus, ChevronUp, ChevronDown } from "lucide-react";
 import { Switch } from "./ui/switch";
+import { escape } from "querystring";
 
 const SurveyBuilder: React.FC = () => {
-  const { elements, addElement, removeElement, updateElement, moveElement } =
-    useSurveyBuilder();
+  const {
+    elements,
+    addElement,
+    removeElement,
+    updateElement,
+    moveElement,
+    selectedElement,
+    setSelectedElement,
+  } = useSurveyBuilder();
 
   return (
-    <div className="flex flex-col gap-4">
+    <div
+      className="flex min-h-screen flex-col gap-4"
+      onClick={() => setSelectedElement(null)}
+    >
       <div className="mx-auto flex w-full max-w-2xl flex-col gap-4">
         <h1 className="text-2xl">Survey Builder</h1>
         {elements.map((element, idx) => (
@@ -22,6 +35,8 @@ const SurveyBuilder: React.FC = () => {
             removeElement={removeElement}
             updateElement={updateElement}
             moveElement={moveElement}
+            selectedElement={selectedElement}
+            setSelectedElement={setSelectedElement}
             idx={idx}
             length={elements.length}
           />
@@ -68,12 +83,23 @@ const BuilderElementWrapper: React.FC<{
   ) => void;
   idx: number;
   length: number;
-}> = ({ element, removeElement, updateElement, moveElement, idx, length }) => {
+  selectedElement: SurveyElementInstance | null;
+  setSelectedElement: (element: SurveyElementInstance | null) => void;
+}> = ({
+  element,
+  removeElement,
+  updateElement,
+  moveElement,
+  idx,
+  length,
+  selectedElement,
+  setSelectedElement,
+}) => {
   return (
-    <div className="flex w-full gap-2" key={element.id}>
+    <div className="relative flex w-full gap-2" key={element.id}>
       <div className="flex flex-col items-center gap-2 p-2">
         <Button
-          variant="outline"
+          variant="ghost"
           onClick={() => moveElement(element.id, "up", element)}
           size="icon"
           disabled={idx === 0}
@@ -84,7 +110,7 @@ const BuilderElementWrapper: React.FC<{
         <p className="font-mono text-muted-foreground">{idx + 1}</p>
 
         <Button
-          variant="outline"
+          variant="ghost"
           onClick={() => moveElement(element.id, "down", element)}
           size="icon"
           disabled={idx === length - 1}
@@ -92,34 +118,51 @@ const BuilderElementWrapper: React.FC<{
           <ChevronDown className="h-4 w-4" />
         </Button>
       </div>
-      <div className="flex w-full flex-col items-center gap-2 border-2 border-muted p-4">
+      <div
+        className={cn(
+          "flex w-full cursor-pointer flex-col items-center gap-2 rounded-md border-2 border-muted p-4 transition-all duration-100 hover:border-primary",
+          selectedElement?.id === element.id && "border-primary",
+        )}
+        onClick={(e) => {
+          e.stopPropagation();
+          setSelectedElement(element);
+        }}
+      >
         {SurveyElements[element.type].editorComponent({
           elementInstance: element,
         })}
-
-        <div className="flex w-full items-center justify-between gap-2">
-          <Button
-            variant="destructive"
-            onClick={() => removeElement(element.id)}
-            className="px-2 py-1"
-          >
-            <Trash className="mr-1 h-4 w-4" />
-            Remove
-          </Button>
-          <Switch
-            checked={element.properties?.required}
-            onCheckedChange={(checked) => {
-              updateElement(element.id, {
-                id: element.id,
-                type: element.type,
-                properties: {
-                  ...element.properties,
-                  required: checked,
-                },
-              });
-            }}
-          />
-        </div>
+        {selectedElement?.id === element.id && (
+          <div className="mt-4 flex w-full items-center justify-between gap-2">
+            <Button
+              variant="outline"
+              onClick={() => removeElement(element.id)}
+              className="border-destructive text-destructive"
+              size="sm"
+            >
+              <Trash className="mr-1 h-4 w-4" color="red" />
+              Remove
+            </Button>
+            <div className="flex items-center gap-2">
+              <Switch
+                id="required"
+                checked={element.properties?.required}
+                onCheckedChange={(checked) => {
+                  updateElement(element.id, {
+                    id: element.id,
+                    type: element.type,
+                    properties: {
+                      ...element.properties,
+                      required: checked,
+                    },
+                  });
+                }}
+              />
+              <Label htmlFor="required" className="font-mono">
+                Required
+              </Label>
+            </div>
+          </div>
+        )}
       </div>
     </div>
   );
