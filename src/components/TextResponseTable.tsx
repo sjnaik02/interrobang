f49c3b57f1979e34d8a4140e669a8a48ddecadff
Shourya Naik <sjnaik02@gmail.com>
2024-11-11 18:17:28 +0530

    cosmetic changes

--- src/components/TextResponseTable.tsx
+++ src/components/TextResponseTable.tsx
@@ -1,5 +1,6 @@
 /* eslint-disable @typescript-eslint/no-unsafe-call */
 import { type Response } from "@/server/db/schema";
+import { CardContent, Card, CardTitle, CardHeader } from "./ui/card";
 import {
   Table,
   TableBody,
@@ -14,37 +15,41 @@ interface TextResponseTableProps {
   question: TextAreaInstance;
   responses: Response[];
 }
-
 export default function TextResponseTable({
   question,
   responses,
 }: TextResponseTableProps) {
   return (
-    <div className="w-full">
-      <h2 className="mb-4 text-xl">{question.properties.label}</h2>
-      <Table>
-        <TableHeader>
-          <TableRow>
-            <TableHead>Created At</TableHead>
-            <TableHead>Response</TableHead>
-          </TableRow>
-        </TableHeader>
-        <TableBody>
-          {responses.map((response, index) => (
-            <TableRow key={index}>
-              <TableCell>
-                {new Date(response.createdAt).toDateString()}
-              </TableCell>
-              <TableCell>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                Officiis rem veniam iure amet doloremque eius recusandae placeat
-                facilis quis laborum pariatur mollitia rerum cupiditate,
-                accusantium excepturi praesentium. Ipsa, dicta nobis.
-              </TableCell>
+    <Card className="w-full">
+      <CardHeader>
+        <CardTitle>{question.properties.label}</CardTitle>
+      </CardHeader>
+      <CardContent>
+        <Table>
+          <TableHeader>
+            <TableRow>
+              <TableHead>Created At</TableHead>
+              <TableHead>Response</TableHead>
             </TableRow>
-          ))}
-        </TableBody>
-      </Table>
-    </div>
+          </TableHeader>
+          <TableBody>
+            {responses.map((response, index) => (
+              <TableRow key={index}>
+                <TableCell className="whitespace-nowrap">
+                  {new Date(response.createdAt).toLocaleDateString()}
+                </TableCell>
+                <TableCell>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Officiis rem veniam iure amet doloremque eius recusandae
+                  placeat facilis quis laborum pariatur mollitia rerum
+                  cupiditate, accusantium excepturi praesentium. Ipsa, dicta
+                  nobis.
+                </TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </CardContent>
+    </Card>
   );
 }
