f0b3936b234381bfc5a2d3753882ef9cee3e3cb0
Shourya Naik <sjnaik02@gmail.com>
2024-11-14 19:54:06 +0530

    dashboard uses custom query

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,5 +1,5 @@
 import { currentUser } from "@clerk/nextjs/server";
-import { getSurveys, getAllResponses } from "@/server/queries";
+import { getDashboardData } from "@/server/queries";
 import Link from "next/link";
 import {
   Table,
@@ -19,8 +19,10 @@ import { ExternalLink, MessageSquareQuote, BarChart } from "lucide-react";
 
 export default async function DashboardPage() {
   const user = await currentUser();
-  const surveys = await getSurveys(0, 14);
-  const responses = await getAllResponses();
+  const { requestedSurveys: surveys, responseCount } = await getDashboardData(
+    4,
+    14,
+  );
   return (
     <div className="flex w-full flex-col gap-2 p-4">
       <header className="flex items-center gap-2">
@@ -29,7 +31,7 @@ export default async function DashboardPage() {
           Welcome back, <span className="">{user?.firstName}</span>
         </p>
       </header>
-      <ResponsesChart responses={responses} />
+      <ResponsesChart responseCount={responseCount} />
       <Card>
         <CardHeader>
           <CardTitle>Your latest surveys</CardTitle>
@@ -49,69 +51,66 @@ export default async function DashboardPage() {
               </TableRow>
             </TableHeader>
             <TableBody>
-              {/* map all non-archived surveys */}
-              {surveys
-                .filter((survey) => !survey.isArchived)
-                .map((survey) => (
-                  <TableRow key={survey.id}>
-                    <TableCell>
+              {surveys.map((survey) => (
+                <TableRow key={survey.id}>
+                  <TableCell>
+                    <Link
+                      href={`/survey/create/${survey.id}`}
+                      className="flex w-full items-center gap-2 hover:underline"
+                    >
+                      {survey.name}
+                      <ExternalLink className="h-4 w-4" />
+                      {new Date().getTime() -
+                        new Date(survey.createdAt).getTime() <
+                        1000 * 60 * 60 * 12 && (
+                        <Badge className="self-end bg-green-400 text-black no-underline hover:bg-green-300">
+                          New
+                        </Badge>
+                      )}
+                    </Link>
+                  </TableCell>
+                  <TableCell>
+                    <Badge
+                      className={`${survey.isPublished ? "bg-green-400 hover:bg-green-400" : "bg-yellow-400 hover:bg-yellow-400"} text-black`}
+                    >
+                      {survey.isPublished ? "Published" : "Draft"}
+                    </Badge>
+                  </TableCell>
+                  <TableCell className="truncate">{survey.title}</TableCell>
+                  <TableCell className="text-sm">
+                    {new Date(survey.createdAt).toLocaleDateString()}
+                  </TableCell>
+                  <TableCell className="text-right tabular-nums">
+                    {survey.responseCount}
+                  </TableCell>
+                  <TableCell className="text-center">
+                    <Button variant="ghost" size="icon" asChild>
                       <Link
-                        href={`/survey/create/${survey.id}`}
-                        className="flex w-full items-center gap-2 hover:underline"
+                        href={`/survey/responses/${survey.id}`}
+                        className="flex items-center gap-2 hover:underline"
                       >
-                        {survey.name}
-                        <ExternalLink className="h-4 w-4" />
-                        {new Date().getTime() -
-                          new Date(survey.createdAt).getTime() <
-                          1000 * 60 * 60 * 12 && (
-                          <Badge className="self-end bg-green-400 text-black no-underline hover:bg-green-300">
-                            New
-                          </Badge>
-                        )}
+                        <MessageSquareQuote className="h-6 w-6" />
                       </Link>
-                    </TableCell>
-                    <TableCell>
-                      <Badge
-                        className={`${survey.isPublished ? "bg-green-400 hover:bg-green-400" : "bg-yellow-400 hover:bg-yellow-400"} text-black`}
+                    </Button>
+                  </TableCell>
+                  <TableCell className="text-center">
+                    <Button variant="ghost" size="icon" asChild>
+                      <Link
+                        href={`/survey/visualize/${survey.id}`}
+                        className="flex items-center gap-2 hover:underline"
                       >
-                        {survey.isPublished ? "Published" : "Draft"}
-                      </Badge>
-                    </TableCell>
-                    <TableCell className="truncate">{survey.title}</TableCell>
-                    <TableCell className="text-sm">
-                      {new Date(survey.createdAt).toLocaleDateString()}
-                    </TableCell>
-                    <TableCell className="text-right tabular-nums">
-                      {survey.responseCount}
-                    </TableCell>
-                    <TableCell className="text-center">
-                      <Button variant="ghost" size="icon" asChild>
-                        <Link
-                          href={`/survey/responses/${survey.id}`}
-                          className="flex items-center gap-2 hover:underline"
-                        >
-                          <MessageSquareQuote className="h-6 w-6" />
-                        </Link>
-                      </Button>
-                    </TableCell>
-                    <TableCell className="text-center">
-                      <Button variant="ghost" size="icon" asChild>
-                        <Link
-                          href={`/survey/visualize/${survey.id}`}
-                          className="flex items-center gap-2 hover:underline"
-                        >
-                          <BarChart className="h-6 w-6" />
-                        </Link>
-                      </Button>
-                    </TableCell>
-                    <TableCell className="text-right">
-                      <SurveyActionsDropdown
-                        id={survey.id}
-                        archiveSurvey={archiveSurvey}
-                      />
-                    </TableCell>
-                  </TableRow>
-                ))}
+                        <BarChart className="h-6 w-6" />
+                      </Link>
+                    </Button>
+                  </TableCell>
+                  <TableCell className="text-right">
+                    <SurveyActionsDropdown
+                      id={survey.id}
+                      archiveSurvey={archiveSurvey}
+                    />
+                  </TableCell>
+                </TableRow>
+              ))}
             </TableBody>
           </Table>
         </CardContent>
