0ad1f5157915e786011af4d77afbc27fd57d61fe
Shourya Naik <sjnaik02@gmail.com>
2024-12-10 22:47:46 +0530

    trying again to fix the layout shift

--- src/app/dashboard/DashboardSidebar.tsx
+++ src/app/dashboard/DashboardSidebar.tsx
@@ -69,8 +69,8 @@ export const DashboardSidebar = ({
               <>
                 <Skeleton className="h-8 w-8 rounded-full" />
                 <div className="flex flex-col gap-1">
-                  <Skeleton className="h-4 w-30" />
-                  <Skeleton className="h-3 w-24" />
+                  <Skeleton className="h-4 w-full" />
+                  <Skeleton className="h-3 w-full" />
                 </div>
               </>
             )}
