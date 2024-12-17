f651e2c9215452dfec52a90ab5f9dbadba7951c8
Shourya Naik <sjnaik02@gmail.com>
2024-12-17 23:16:02 +0530

    Rendering a basic list of org members

--- src/app/dashboard/users/page.tsx
+++ src/app/dashboard/users/page.tsx
@@ -1,9 +1,16 @@
+/* eslint-disable @next/next/no-img-element */
 "use client";
 
 import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert";
 import { Building } from "lucide-react";
+import { useOrganization, useUser } from "@clerk/nextjs";
 
 export default function UsersPage() {
+  const { organization } = useOrganization();
+  const { user } = useUser();
+  if (user?.firstName !== "Shourya") {
+    return <div>You are not authorized to access this page</div>;
+  }
   return (
     <div className="flex min-h-screen w-full flex-col gap-4 p-4">
       <h1 className="text-2xl">Users</h1>
@@ -14,6 +21,36 @@ export default function UsersPage() {
           User management is under development.
         </AlertDescription>
       </Alert>
+      <div className="flex flex-col gap-4">
+        <h2 className="text-lg">Organization Users</h2>
+        <div className="flex flex-col gap-2">
+          {organization?.getMemberships().then((memberships) => {
+            console.log(memberships);
+            return memberships.data.map((membership) => (
+              <div key={membership.id} className="flex items-center gap-2">
+                {membership.publicUserData.hasImage ? (
+                  <img
+                    src={membership.publicUserData.imageUrl}
+                    alt={
+                      membership.publicUserData.firstName ?? "Profile picture"
+                    }
+                    width={40}
+                    height={40}
+                    className="rounded-full"
+                  />
+                ) : (
+                  <div className="h-10 w-10 rounded-full bg-pink-500" />
+                )}
+                <span>
+                  {membership.publicUserData.firstName +
+                    " " +
+                    membership.publicUserData.lastName}
+                </span>
+              </div>
+            ));
+          })}
+        </div>
+      </div>
     </div>
   );
 }
