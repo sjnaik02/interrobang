f899050efa0b4559c11285e51867324ae1e566e7
Shourya Naik <sjnaik02@gmail.com>
2025-05-15 17:27:51 +0530

    fix: another build error similar to prev

--- src/components/SurveyPreview.tsx
+++ src/components/SurveyPreview.tsx
@@ -12,14 +12,15 @@ export default function SurveyPreview({ survey }: SurveyPreviewProps) {
   return (
     <div className="flex flex-col gap-4">
       <h1 className="text-3xl font-medium">{survey.title}</h1>
-      {survey.questions?.map((element: SurveyElementInstance, idx: number) => (
-        <div key={idx + element.type} className="flex w-full">
-          <p className="mr-2 text-lg">{idx + 1}. </p>
-          {SurveyElements[element.type].previewComponent({
-            elementInstance: element,
-          })}
-        </div>
-      ))}
+      {survey.questions?.map((element: SurveyElementInstance, idx: number) => {
+        const PreviewComponent = SurveyElements[element.type].previewComponent;
+        return (
+          <div key={idx + element.type} className="flex w-full">
+            <p className="mr-2 text-lg">{idx + 1}. </p>
+            <PreviewComponent elementInstance={element} />
+          </div>
+        );
+      })}
       <Button variant="default" className="mt-4 w-fit">
         Submit
       </Button>
